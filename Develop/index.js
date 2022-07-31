a58a7931ce3ae9e155504584e40b366afba15c54
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-07-31 11:09:56 -0500

    added dependencies and created index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,88 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// An array of questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's name?",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Please write a short description of your project : ",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your github username?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+      },
+      {
+        type: "list",
+        message: "What kind of license should your project have?",
+        name: "license",
+        choices: [
+          "MIT License",
+          "Apache License 2.0",
+          "GNU General Public License 3.0",
+          "BSD 3 License",
+          "None",
+        ],
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: "What command should be run to install dependencies?",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: "What is the usage information?",
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message:
+          "What are the contribution guidelines?",
+      }
+    ];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// A function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), error => {
+    
+        console.log("Generating README...");
+    
+        if (error) throw error;
+    
+        console.log("README file Generated!");
+      });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// A function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(function (answers) {
+      writeToFile("README.md", answers);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
 // Function call to initialize app
 init();
